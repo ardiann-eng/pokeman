d125703fea0dc0649689da88d66bcf8fcf10b305
ardiann-eng <139459108+ardiann-eng@users.noreply.github.com>
2025-09-04 01:26:57 +0800

    Update game.js

--- game.js
+++ game.js
@@ -901,43 +901,6 @@ class PokemonPacman {
     }
 }
 
-// Polyfills for older browsers
-(function() {
-    // RequestAnimationFrame polyfill
-    let lastTime = 0;
-    const vendors = ['ms', 'moz', 'webkit', 'o'];
-    for(let x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
-                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
-    }
- 
-    if (!window.requestAnimationFrame)
-        window.requestAnimationFrame = function(callback) {
-            const currTime = new Date().getTime();
-            const timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            const id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
-              timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
- 
-    if (!window.cancelAnimationFrame)
-        window.cancelAnimationFrame = function(id) {
-            clearTimeout(id);
-        };
-        
-    // Performance.now polyfill
-    if (!window.performance) {
-        window.performance = {};
-    }
-    if (!window.performance.now) {
-        window.performance.now = function() {
-            return Date.now();
-        };
-    }
-})();
-
 // Start the game when page loads
 window.addEventListener('load', () => {
     new PokemonPacman();
